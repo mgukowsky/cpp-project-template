75323707ed18c26f3ab2d95e3ffe3c73df0ddc75
Matt Gukowsky <mgukowsky@gmail.com>
2024-08-27 23:57:31 -0400

    Fix CMake Linker spec; use latest CPM

--- cmake/GetCPM.cmake
+++ cmake/GetCPM.cmake
@@ -1,8 +1,10 @@
 # Per https://github.com/cpm-cmake/CPM.cmake/wiki/Downloading-CPM.cmake-in-CMake
 file(
   DOWNLOAD
-  https://github.com/cpm-cmake/CPM.cmake/releases/download/v0.40.2/CPM.cmake
+  # https://github.com/cpm-cmake/CPM.cmake/releases/download/v0.40.2/CPM.cmake
+  https://github.com/cpm-cmake/CPM.cmake/releases/latest/download/CPM.cmake
   ${CMAKE_CURRENT_BINARY_DIR}/cmake/CPM.cmake
-  EXPECTED_HASH
-    SHA256=c8cdc32c03816538ce22781ed72964dc864b2a34a310d3b7104812a5ca2d835d)
+  # EXPECTED_HASH
+  #   SHA256=c8cdc32c03816538ce22781ed72964dc864b2a34a310d3b7104812a5ca2d835d
+)
 include(${CMAKE_CURRENT_BINARY_DIR}/cmake/CPM.cmake)
