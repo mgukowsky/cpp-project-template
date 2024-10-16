24e0c0eafa7237422fded3c25f651dcb2a2d0460
Matt Gukowsky <mgukowsky@gmail.com>
2024-10-15 23:14:13 -0400

    Add MSVC toolchain

--- tasks.py
+++ tasks.py
@@ -1,19 +1,37 @@
 from invoke import task, Context
 import os
+import platform
 
-DEFAULT_TOOLCHAIN="clang_debug"
+
+def default_toolchain():
+    system = platform.system()
+    if system == "Windows":
+        return "msvc_debug"
+    elif system == "Linux":
+        return "clang_debug"
+    else:
+        raise RuntimeError("Could not determine default toolchain for system")
+
 
 def link_ccdb(preset: str):
-    os.symlink(f"{os.getcwd()}/build/{preset}/compile_commands.json", "compile_commands.json.new")
+    os.symlink(
+        f"{os.getcwd()}/build/{preset}/compile_commands.json",
+        "compile_commands.json.new",
+    )
     os.replace("compile_commands.json.new", "compile_commands.json")
 
+
 @task
-def build(c: Context, preset: str = DEFAULT_TOOLCHAIN):
-    c.run(f"cmake --preset {preset} && cmake --build --preset {preset}", echo=True, pty=True) 
+def build(c: Context, preset: str = default_toolchain()):
+    c.run(
+        f"cmake --preset {preset} && cmake --build --preset {preset}",
+        echo=True,
+        pty=True,
+    )
     link_ccdb(preset)
 
+
 @task(default=True)
-def workflow(c: Context, preset: str = DEFAULT_TOOLCHAIN):
-    # TODO: extrapolate default workflow based on platform
-    c.run(f"cmake --workflow --preset {preset}", echo=True, pty=True) 
+def workflow(c: Context, preset: str = default_toolchain()):
+    c.run(f"cmake --workflow --preset {preset}", echo=True, pty=True)
     link_ccdb(preset)
