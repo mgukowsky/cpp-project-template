47a66efdd709210921e9f3a5c5e4d05edf821674
Matt Gukowsky <mgukowsky@gmail.com>
2024-11-24 17:02:12 -0500

    Add TypeString

---
+++ include/mgfw/TypeString.hpp
@@ -0,0 +1,66 @@
+#pragma once
+
+#include <iostream>
+#include <string_view>
+#include <type_traits>
+#include <vector>
+
+namespace mgfw {
+
+namespace detail_ {
+  consteval std::string_view typeStringCommon(std::string_view  rawString,
+                                              const std::size_t startIdx,
+                                              const std::size_t endIdx) {
+    return rawString.substr(startIdx, endIdx);
+  }
+
+  /**
+   * GCC and clang both place the typename of the type given as T in metastring two characters after
+   * and '=' sign (e.g. "... T = "), with a small difference in the first character that comes after
+   * the type name.
+   */
+  consteval std::string_view gccTypeString(std::string_view rawString,
+                                           const char       terminalChar = ';') {
+    const std::size_t startChar = rawString.find_first_of('=') + 2;
+    const std::size_t endChar   = rawString.find_first_of(terminalChar);
+
+    return typeStringCommon(rawString, startChar, endChar - startChar);
+  }
+
+  /**
+   * MSVC's mangling is slightly more verbose. The type name will be prefixed by 'metastring',
+   * followed by a '<' character, and will end will a '>)' sequence, which accounts for the +/- 1
+   * offsets in the two indices.
+   */
+  consteval std::string_view msvcTypeString(std::string_view rawString) {
+    const std::string_view startString = "metastring";
+    const std::size_t      startIdx    = rawString.find(startString) + startString.size() + 1;
+    const std::size_t      endIdx      = rawString.find_first_of('(') - 1;
+
+    return rawString.substr(startIdx, endIdx - startIdx);
+  }
+
+  /**
+   * Most compilers have a magical macro that can return the name of the enclosing function,
+   * including template info.
+   */
+  template<typename T>
+  consteval std::string_view metastring() {
+#if defined(__clang__)
+    return gccTypeString(std::string_view(__PRETTY_FUNCTION__), ']');
+#elif defined(__GNUC__)
+    return gccTypeString(std::string_view(__PRETTY_FUNCTION__));
+#elif defined(_MSC_VER)
+    return msvcTypeString(__FUNCSIG__);
+#error "This compiler cannot support the compile-time type string feature."
+#endif
+  }
+}  // namespace detail_
+
+/**
+ * Return the name of a given type as a string_view, computed at compile time.
+ */
+template<typename Raw_t, typename T = std::remove_pointer_t<std::decay_t<Raw_t>>>
+constinit const inline std::string_view TypeString = detail_::metastring<T>();
+
+}  // namespace mgfw
---
+++ test/unit/TypeString_test.cpp
@@ -0,0 +1,28 @@
+#include "mgfw/TypeString.hpp"
+
+#include <gtest/gtest.h>
+
+#include <string>
+
+class Klass { };
+
+using mgfw::TypeString;
+
+TEST(TypeString_test, getTypeStrings) {
+  EXPECT_STREQ("int", std::string(TypeString<int>).c_str())
+    << "TypeString<T> correctly generates a string with the name of T";
+  EXPECT_STREQ("int", std::string(TypeString<int &>).c_str())
+    << "TypeString<T> correctly generates a string with the name of T";
+  EXPECT_STREQ("const int", std::string(TypeString<const int * const>).c_str())
+    << "TypeString<T> correctly generates a string with the name of T";
+
+// MSVC mangles class names a little bit differently than what these two tests check for, however
+// the names of the types are still clear.
+#if defined(__clang__) || defined(__GNUC__)
+  EXPECT_STREQ("Klass", std::string(TypeString<Klass>).c_str())
+    << "TypeString<T> correctly generates a string with the name of T";
+  EXPECT_STREQ("std::vector<const Klass>",
+               std::string(TypeString<const std::vector<const Klass>>).c_str())
+    << "TypeString<T> correctly generates a string with the name of T";
+#endif
+}
