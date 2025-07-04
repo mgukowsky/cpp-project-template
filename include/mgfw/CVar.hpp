15cc909f66e7b9beb221259794e7c1a3b2a232e6
Matt Gukowsky <mgukowsky@gmail.com>
2025-07-04 14:12:46 -0400

    Clean up CVar and add on_change callbacks

--- include/mgfw/CVar.hpp
+++ include/mgfw/CVar.hpp
@@ -1,10 +1,9 @@
 #pragma once
 
-#include "mgfw/TypeString.hpp"
-
+#include <functional>
 #include <string>
 #include <utility>
-#include <variant>
+#include <vector>
 
 namespace mgfw {
 
@@ -12,45 +11,42 @@ template<typename T>
 concept is_cvar_t = std::same_as<T, int> || std::same_as<T, double> || std::same_as<T, bool>
                  || std::same_as<T, std::string>;
 
+template<typename T>
+requires is_cvar_t<T>
 class CVar {
 public:
-  using Value_t = std::variant<int, double, bool, std::string>;
+  using CVarCallback_t = std::function<void(const T &prevVal, const T &newVal)>;
 
-  CVar(std::string name, Value_t initialVal, std::string desc = "")
+  CVar(std::string name, const T initialVal, std::string desc = "")
     : val_(std::move(initialVal)), name_(std::move(name)), desc_(std::move(desc)) { }
 
   const std::string &name() const { return name_; }
 
   const std::string &desc() const { return desc_; }
 
-  // Get value as T; throws if wrong type.
-  // Eventually may write something to coerce to the requested type
-  template<typename T>
-  requires is_cvar_t<T>
-  const T &get() const {
-    assert_has_type_<T>();
-    return std::get<T>(val_);
-  }
+  const T &get() const { return val_; }
 
-  // Set value as T; throws if wrong type.
-  template<typename T>
-  requires is_cvar_t<T>
+  void on_change(CVarCallback_t callback) { onChangeCallbacks_.emplace_back(std::move(callback)); }
+
+  // Set value.
+  // After setting the new value, invoke any callbacks, passing the previous value and the new
+  // value.
   void set(const T &val) {
-    assert_has_type_<T>();
-    val_ = val;
-  }
+    const T prev = val_;
 
-private:
-  template<typename T>
-  requires is_cvar_t<T>
-  void assert_has_type_() const {
-    if(!std::holds_alternative<T>(val_)) {
-      throw std::runtime_error(
-        std::format("Expected {}, but type index was {} ({})", TypeString<T>, val_.index(), name_));
+    val_ = val;
+
+    if(prev != val_) {
+      for(const auto &callback : onChangeCallbacks_) {
+        callback(prev, val_);
+      }
     }
   }
 
-  Value_t val_;
+private:
+  T val_;
+
+  std::vector<CVarCallback_t> onChangeCallbacks_;
 
   const std::string name_;
   const std::string desc_;
--- test/unit/CVar_test.cpp
+++ test/unit/CVar_test.cpp
@@ -2,85 +2,69 @@
 
 #include <gtest/gtest.h>
 
-#include <stdexcept>
 #include <string>
 
 using mgfw::CVar;
 
 // Construction and correct get
 TEST(CVarTest, ConstructAndGetInt) {
-  CVar cvar("test_int", 42, "desc");
+  CVar<int> cvar("test_int", 42, "desc");
   EXPECT_EQ(cvar.name(), "test_int");
   EXPECT_EQ(cvar.desc(), "desc");
-  EXPECT_EQ(cvar.get<int>(), 42);
+  EXPECT_EQ(cvar.get(), 42);
 }
 
 TEST(CVarTest, ConstructAndGetDouble) {
-  CVar cvar("test_double", 3.1415, "desc");
-  EXPECT_DOUBLE_EQ(cvar.get<double>(), 3.1415);
+  CVar<double> cvar("test_double", 3.1415, "desc");
+  EXPECT_DOUBLE_EQ(cvar.get(), 3.1415);
 }
 
 TEST(CVarTest, ConstructAndGetBool) {
-  CVar cvar("test_bool", true, "desc");
-  EXPECT_EQ(cvar.get<bool>(), true);
+  CVar<bool> cvar("test_bool", true, "desc");
+  EXPECT_EQ(cvar.get(), true);
 }
 
 TEST(CVarTest, ConstructAndGetString) {
-  CVar cvar("test_str", std::string("hello"), "desc");
-  EXPECT_EQ(cvar.get<std::string>(), "hello");
+  CVar<std::string> cvar("test_str", std::string("hello"), "desc");
+  EXPECT_EQ(cvar.get(), "hello");
 }
 
 // Correct set (same type)
 TEST(CVarTest, SetSameType) {
-  CVar cvar("test", 1);
-  cvar.set<int>(42);
-  EXPECT_EQ(cvar.get<int>(), 42);
+  CVar<int> cvar("test", 1);
+  cvar.set(42);
+  EXPECT_EQ(cvar.get(), 42);
 
-  CVar cvar2("test2", std::string("foo"));
-  cvar2.set<std::string>("bar");
-  EXPECT_EQ(cvar2.get<std::string>(), "bar");
+  CVar<std::string> cvar2("test2", std::string("foo"));
+  cvar2.set("bar");
+  EXPECT_EQ(cvar2.get(), "bar");
 }
 
-// Throws on get with wrong type
-TEST(CVarTest, GetWrongTypeThrows) {
-  CVar cvar("test", 42);
-  EXPECT_THROW(cvar.get<std::string>(), std::runtime_error);
-  EXPECT_THROW(cvar.get<double>(), std::runtime_error);
-  EXPECT_THROW(cvar.get<bool>(), std::runtime_error);
+TEST(CVarTest, SetInvokesCallbacks) {
+  CVar<int> cvar("test", 1);
+  int       i = 0;
+  cvar.on_change([&](const int &prevVal, const int &newVal) {
+    EXPECT_EQ(1, prevVal);
+    EXPECT_EQ(42, newVal);
+    i = 1234;
+  });
+  cvar.set(42);
+  EXPECT_EQ(cvar.get(), 42);
+  EXPECT_EQ(1234, i);
 }
 
-// Throws on set with wrong type
-TEST(CVarTest, SetWrongTypeThrows) {
-  CVar cvar("test", 42);
-  EXPECT_THROW(cvar.set<double>(3.14), std::runtime_error);
-  EXPECT_THROW(cvar.set<std::string>("abc"), std::runtime_error);
-  EXPECT_THROW(cvar.set<bool>(true), std::runtime_error);
+TEST(CVarTest, SkipCallbackIfValueIsUnchanged) {
+  CVar<int> cvar("test", 1);
+  int       i = 0;
+  cvar.on_change(
+    [&]([[maybe_unused]] const int &prevVal, [[maybe_unused]] const int &newVal) { i += 1000; });
+  cvar.set(42);
+  EXPECT_EQ(1000, i);
 
-  CVar cvar2("test2", std::string("foo"));
-  EXPECT_THROW(cvar2.set<int>(7), std::runtime_error);
-  EXPECT_THROW(cvar2.set<bool>(false), std::runtime_error);
-}
+  cvar.set(84);
+  EXPECT_EQ(2000, i);
 
-// Throws on set after type change attempt
-TEST(CVarTest, SetDoesNotChangeType) {
-  CVar cvar("test", false);
-  EXPECT_THROW(cvar.set<double>(2.0), std::runtime_error);
-  EXPECT_EQ(cvar.get<bool>(), false);
-
-  CVar cvar2("test2", 1.23);
-  EXPECT_THROW(cvar2.set<int>(5), std::runtime_error);
-  EXPECT_EQ(cvar2.get<double>(), 1.23);
-}
-
-// Throws with informative error message
-TEST(CVarTest, ErrorMessageContainsNameAndType) {
-  CVar cvar("myvar", 10, "desc");
-  try {
-    cvar.get<double>();
-  }
-  catch(const std::runtime_error &err) {
-    std::string msg = err.what();
-    EXPECT_NE(msg.find("myvar"), std::string::npos);
-    EXPECT_NE(msg.find("double"), std::string::npos);
-  }
+  cvar.set(84);
+  EXPECT_EQ(2000, i)
+    << "on_change callbacks should be skipped if the value does not actually change";
 }
