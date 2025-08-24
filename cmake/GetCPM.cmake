191faf5a4f8da9be3b8eabe97fd0cbb88db7b0dd
Matt Gukowsky <mgukowsky@gmail.com>
2025-08-24 19:43:29 -0400

    Respect CPM_SOURCE_CACHE, and cache CPM.cmake itself

--- cmake/GetCPM.cmake
+++ cmake/GetCPM.cmake
@@ -1,12 +1,23 @@
 set(FETCHCONTENT_QUIET FALSE)
 
-# Per https://github.com/cpm-cmake/CPM.cmake/wiki/Downloading-CPM.cmake-in-CMake
-file(
-  DOWNLOAD
-  # https://github.com/cpm-cmake/CPM.cmake/releases/download/v0.40.2/CPM.cmake
-  https://github.com/cpm-cmake/CPM.cmake/releases/latest/download/CPM.cmake
-  ${CMAKE_CURRENT_BINARY_DIR}/cmake/CPM.cmake
-  # EXPECTED_HASH
-  #   SHA256=c8cdc32c03816538ce22781ed72964dc864b2a34a310d3b7104812a5ca2d835d
-)
-include(${CMAKE_CURRENT_BINARY_DIR}/cmake/CPM.cmake)
+if(DEFINED ENV{CPM_SOURCE_CACHE})
+  set(CPM_DOWNLOAD_FILE $ENV{CPM_SOURCE_CACHE}/CPM.cmake)
+else()
+  set(CPM_DOWNLOAD_FILE ${CMAKE_CURRENT_BINARY_DIR}/cmake/CPM.cmake)
+endif()
+
+# In the event that we're offline, CMake's idiotic behavior is to 
+# replace the destination with an empty file, even if the file is
+# non-empty!
+if(NOT EXISTS "${CPM_DOWNLOAD_FILE}")
+  # Per https://github.com/cpm-cmake/CPM.cmake/wiki/Downloading-CPM.cmake-in-CMake
+  file(
+    DOWNLOAD
+    # https://github.com/cpm-cmake/CPM.cmake/releases/download/v0.40.2/CPM.cmake
+    https://github.com/cpm-cmake/CPM.cmake/releases/latest/download/CPM.cmake
+    ${CPM_DOWNLOAD_FILE}
+    # EXPECTED_HASH
+    #   SHA256=c8cdc32c03816538ce22781ed72964dc864b2a34a310d3b7104812a5ca2d835d
+  )
+endif()
+include(${CPM_DOWNLOAD_FILE})
